c40dc81176569ad10ed341dcca7d9186ba9327b3
Milton Leung <milleung964@gmail.com>
2020-05-28 22:43:58 -0700

    remove autoscroll, set up offset listeners

--- StockTickerDisplay/ContentView.swift
+++ StockTickerDisplay/ContentView.swift
@@ -14,46 +14,97 @@ struct ContentView: View {
   var cellWidth: CGFloat = 150.0 // Update to actual width
   @State var startingIndex: Int = 0
 
-  @State private var resetViews = false
+  @State private var cellSizes: [CGSize] = []
+  @State private var cellSize: CGSize = CGSize.zero
 
-  @State private var startAnimateAutoScroll = true
-  @State private var animateAutoScroll = false
+  //  @State private var resetViews = false
 
-  @State private var horizontalDragAmount: CGFloat = 0
+  //  @State private var startAnimateAutoScroll = true
+  //  @State private var animateAutoScroll = false
+
+  @State private var horizontalDragAmount: CGFloat = 10
+  @State private var offset: CGFloat = 0
 
   var body: some View {
-    let binding = Binding<Bool>(get: { self.resetViews }, set: { self.updateBinding($0) })
+    //    let binding = Binding<Bool>(get: { self.resetViews }, set: { self.updateBinding($0) })
 
-    return HStack {
-      if (!stocks.isEmpty) {
-        ForEach(0..<cellsToShow) {
-          ItemView(stock: self.stocks[($0 + self.startingIndex) % self.stocks.count])
-            .frame(width: self.cellWidth)
+    func makeHorizontalStack() -> some View {
+      HStack {
+        ForEach(0..<self.cellsToShow) { index in
+          ItemView(stock: self.stocks[(index + self.startingIndex) % self.stocks.count], index: (index + self.startingIndex) % self.stocks.count)
+            .frame(width: 150, height: 150)
+        }
+
+      }
+    }
+
+    return
+      Group {
+        GeometryReader { geoProxy in
+          if (!self.stocks.isEmpty) {
+            makeHorizontalStack()
+              .padding(5)
+
+              //                .modifier(ScrollEffect(resetViews: binding, offset: -self.cellWidth, translationX: self.animateAutoScroll ? -self.cellWidth : 0))
+              .preference(key: HorizontalOffsetPreferenceKey.self, value: geoProxy.frame(in: .global).minX)
+
+              .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+              .gesture(
+                DragGesture(coordinateSpace: .global)
+                  .onChanged {
+                    //                    print($0.translation)
+                    //                    self.isAutoScrollAnimating = false
+                    self.horizontalDragAmount = $0.translation.width
+                    //                  if (self.horizontalDragAmount == -150) {
+                    //                    self.horizontalDragAmount = 0
+                    //                    //          self.startingIndex += 1
+                    //                  }
+                    self.offset = self.horizontalDragAmount.truncatingRemainder(dividingBy: 150)
+                }
+                .onEnded {_ in
+                  self.horizontalDragAmount = 0;
+                }
+            )
+              .onPreferenceChange(HorizontalOffsetPreferenceKey.self) { preferences in
+                print("offset: \(preferences)")
+                //                guard let offset = preferences else { return }
+                if (self.horizontalDragAmount == -150) {
+                  self.horizontalDragAmount = 0
+                  //          self.startingIndex += 1
+                }
+
+            }
+            //            .onPreferenceChange(ItemSizePreferenceKey.self) { (preferences: [ItemSizePreferenceData]) in
+            //              if (self.cellSizes.count != self.stocks.count) {
+            //                self.cellSizes = Array<CGSize>(repeating: CGSize.zero, count: self.stocks.count)
+            //              }
+            //              for itemData in preferences {
+            //                self.cellSizes[itemData.index] = itemData.size
+            //              }
+            //              self.cellSize = self.cellSizes[0]
+            //              print(self.cellSizes)
+            //            }
+          }
         }
       }
-    }
-    .padding(5)
-    .offset(x: horizontalDragAmount, y: 0)
-    .modifier(ScrollEffect(resetViews: binding, offset: -cellWidth, translationX: animateAutoScroll ? -cellWidth : 0))
-    .onAppear() {
-      withAnimation(self.startAnimateAutoScroll ? Animation.linear(duration: 2).repeatForever(autoreverses: false) : .default) {
-        self.animateAutoScroll = true;
-      }
-    }
-//    .gesture(
-//      DragGesture()
-//        .onChanged {
-//          self.startAnimateAutoScroll = false
-//          self.horizontalDragAmount = $0.translation.width }
-//    )
-      .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+      .offset(x: self.offset, y: 0)
   }
 
-  func updateBinding(_ value: Bool) {
-    if resetViews != value && !resetViews {
-      startingIndex += 1
-    }
-    resetViews = value
+  //  func updateBinding(_ value: Bool) {
+  //    if resetViews != value && !resetViews {
+  //      startingIndex += 1
+  //    }
+  //    resetViews = value
+  //  }
+}
+
+struct HorizontalOffsetPreferenceKey: PreferenceKey {
+  typealias Value = CGFloat
+
+  static var defaultValue: Value = 0
+
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
   }
 }
 
--- StockTickerDisplay/ItemView.swift
+++ StockTickerDisplay/ItemView.swift
@@ -10,20 +10,41 @@ import SwiftUI
 
 struct ItemView: View {
   var stock: Stock
-  
+  var index: Int
+
   var body: some View {
     HStack {
       VStack(alignment: .leading) {
         Text(stock.symbol.uppercased())
         Text("$\(stock.lastPrice)")
-      }
+      }.border(Color.black)
       Spacer().border(Color.black)
     }
+    .background(
+      GeometryReader { geometry in
+        return Color.clear.preference(key: ItemSizePreferenceKey.self, value: [ItemSizePreferenceData(index: self.index, size: geometry.size)])
+    })
   }
 }
 
+struct ItemSizePreferenceData: Equatable {
+  let index: Int
+  let size: CGSize
+}
+
+struct ItemSizePreferenceKey: PreferenceKey {
+  typealias Value = [ItemSizePreferenceData]
+
+  static var defaultValue: Value = []
+
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value.append(contentsOf: nextValue())
+  }
+}
+
+
 struct ItemView_Previews: PreviewProvider {
   static var previews: some View {
-    ItemView(stock: testStockData[0])
+    ItemView(stock: testStockData[0], index: 0)
   }
 }
